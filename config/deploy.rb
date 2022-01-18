7890a406bca156c6fca5408b7d8cc5babe7faeb8
Shawn <limhaileongshawn@gmail.com>
2022-01-18 12:47:55 +0800

    arg

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.16.0"
 set :application, "task_manager"
-append :linked_files, "config/application.yml"
+set :linked_files, fetch(:linked_files, []).push('config/database.yml')
 set :repo_url, "git@github.com:seadragon2000341/cvwo_backend.git"
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 set :rvm_ruby_version, '3.0.3'
